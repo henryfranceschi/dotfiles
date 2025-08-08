5bb47a69a836097a756aa9e9945172e7d90570fc
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2025-08-08 09:38:05 +0200

    added neovim treesitter-context plugin

--- nvim/lua/henry/plugins/treesitter.lua
+++ nvim/lua/henry/plugins/treesitter.lua
@@ -1,27 +1,37 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
 
-            highlight = {
+                highlight = {
 
-                enable = true,
+                    enable = true,
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                }
             }
-        }
-    end,
+        end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = function()
+            require('treesitter-context').setup {
+                enable = true,
+            }
+        end,
+    },
 }
