484b7a11211bdc94ac59884dc6f785ea61cdeadd
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2025-10-29 08:33:23 +0100

    fixed trouble key mappings

--- nvim/lua/henry/plugins/trouble.lua
+++ nvim/lua/henry/plugins/trouble.lua
@@ -1,14 +1,17 @@
 return {
     'folke/trouble.nvim',
+    opts = {},
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
-        require('trouble').setup {}
+        local trouble = require('trouble')
+        trouble.setup {}
+        -- trouble.toggle(trouble.diagnostics)
 
         local opts = { silent = true, noremap = true }
-        vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', opts)
-        vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', opts)
-        vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', opts)
-        vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', opts)
-        vim.keymap.set('n', '<leader>xr', '<cmd>TroubleToggle lsp_references<cr>', opts)
+        vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', opts)
+        vim.keymap.set('n', '<leader>xw', '<cmd>Trouble workspace_diagnostics toggle<cr>', opts)
+        vim.keymap.set('n', '<leader>xd', '<cmd>Trouble document_diagnostics toggle<cr>', opts)
+        vim.keymap.set('n', '<leader>xq', '<cmd>Trouble quickfix toggle<cr>', opts)
+        vim.keymap.set('n', '<leader>xr', '<cmd>Trouble lsp_references toggle<cr>', opts)
     end,
 }
