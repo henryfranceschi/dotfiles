aeda98fb21d712acaf0c66eeec21b897a9bfaa75
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2025-12-29 15:48:47 +0100

    cleaned up init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,7 @@
 require("config")
 
+-- Disable netrw as we are using nvim-tree.
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.cmd.colorscheme "catppuccin"
-
--- vim.lsp.config("*", {
---     capabilities = vim.lsp.protocol.make_client_capabilities()
--- })
-
--- Extend the config.
--- vim.lsp.config('rust_analyzer', {})
--- vim.lsp.enable('rust_analyzer')
