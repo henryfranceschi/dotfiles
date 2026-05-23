f08c6ad8083dc2002e8ad77e20a37fc57161d84a
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2026-05-23 11:04:20 +0200

    added gitsigns plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,7 +8,27 @@ return {
             "nvim-lua/plenary.nvim",
         },
         keys = {
-            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit" },
+            {
+                "<leader>gg",
+                "<cmd>Neogit<cr>",
+                desc = "Show Neogit"
+            },
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        lazy = false,
+        keys = {
+            {
+                "<leader>gb",
+                "<cmd>Gitsigns toggle_current_line_blame<cr>",
+                desc = "Git toggle blame",
+            },
+            {
+                "<leader>gs",
+                "<cmd>Gitsigns stage_hunk<cr>",
+                desc = "Git stage hunk",
+            },
         },
     },
 }
